9e775efec93425e6e1db297404d9c6ba5d3215cf
Vigo Walker <vigopaul05@gmail.com>
2026-04-10 21:51:35 +0200

    Update environment variable names in .env.example and add example script for placing orders

---
+++ examples/place_order.py
@@ -0,0 +1,47 @@
+import asyncio
+import dotenv
+import os
+
+import BinomoAPI
+from BinomoAPI.exceptions import AuthenticationError
+
+dotenv.load_dotenv()
+
+EMAIL = os.getenv("BINOMO_EMAIL")
+PASSWORD = os.getenv("BINOMO_PASSWORD")
+
+
+async def main():
+    try:
+        login_response = BinomoAPI.login(EMAIL, PASSWORD)
+        session = login_response._session
+        device_id = session.cookies.get("device_id")
+        print(f"Device ID: {device_id}")
+
+        async with BinomoAPI(
+            auth_token=login_response.authtoken,
+            demo=True,
+            enable_logging=True,
+            device_id = device_id
+        ) as api:
+            
+            balance = await api.get_balance()
+            print(f"Balance: ${balance.amount:.2f}")
+            
+            result = await api.place_call_option(
+                asset="EURO",
+                duration_seconds=60,
+                amount=1000,
+                use_demo=True
+            )
+
+            print(f"Trade placed: {result}")
+
+            await asyncio.sleep(15) 
+            balance = await api.get_balance()
+            print(f"Balance after trade: ${balance.amount:.2f}")
+            
+    except AuthenticationError as e:
+        print(f"Login failed: {e}")
+
+asyncio.run(main())
